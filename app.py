b8aa3e2e873b9ea32962b0b42739f2550f768ae4
KarenFFog <124896116+KarenFFog@users.noreply.github.com>
2023-06-06 19:59:28 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -1,7 +1,71 @@
 # from flask import Flask, render_template
 # from flask_googlemaps import GoogleMaps
 # from flask_googlemaps import Map
+import json
+import requests
+import os
+from flask import Flask, render_template, request, jsonify, redirect, url_for
+#from flask_googlemaps import GoogleMaps, Map
 
+
+from pydantic import BaseModel, validator
+
+
+app = Flask(__name__)
+
+
+# "https://1.bp.blogspot.com/-sTxAHAxirGM/WVbAe2098nI/AAAAAAABENs/_I5sYMYgLOUzaIE7FfF4qdGX-hoAkq9SgCLcBGAs/s1600/Blog_20170624_113552.jpg"
+
+######################
+#app.register_blueprint(views, url_prefix="/")
+@app.route('/')
+def home():
+    return render_template ('hej.html')
+
+@app.route('/form', methods=['GET', 'POST'])
+def fill():
+    return render_template ('fill.html')
+
+@app.route('/user/user/Karen')
+def go_back():
+    return redirect(url_for('user', name='Karen'))
+
+
+@app.route('/user', methods=['GET', 'POST'])
+def user():
+    if request.method == 'POST':
+        print(request.form.getlist('check'))
+        # return render_template(fill.html)
+        # Print the form data to the console
+        # for key, value in request.form.items():
+        #     print(f'{key}: {value}')
+    return render_template('please.html')
+
+
+
+# access json
+@app.route("/json")
+def get_json():
+    return jsonify({"name": "Tim", 'age': 21, 'hobbies': ['coding', 'reading', 'gaming']})
+
+# access data from json
+@app.route("/data")
+def get_data():
+    data = request.json
+    return jsonify(data)
+
+@app.route("/go-to-home")
+def go_to_home():
+    return redirect(url_for('home'))
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
+
+
+######################## GOOGLE MAPS - virker ik ##########################################
 # #app = Flask(__name__) 
 # #app.config['GOOGLEMAPS_KEY'] = "8JZ7i18MjFuM35dJHq70n3Hx4"
 
@@ -59,96 +123,8 @@
 
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-import json
-import requests
-from flask import Flask, render_template, request
-from flask_googlemaps import GoogleMaps, Map
+
+
 # require Flask-GoogleMaps (https://github.com/rochacbruno/Flask-GoogleMaps)
+# api_key = 'AIzaSyAzeggzDErZuRAL6B4P1eIOxiYZouYLxu8' # change this to your api key
 
-app = Flask(__name__)
-api_key = 'AIzaSyAzeggzDErZuRAL6B4P1eIOxiYZouYLxu8' # change this to your api key
-# get api key from Google API Console (https://console.cloud.google.com/apis/)
-GoogleMaps(app, key=api_key) # set api_key
-devices_data = {} # dict to store data of devices
-devices_location = {} # dict to store coordinates of devices
-# use sqlalchemy or something to store things in database
-
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    # json_data = request.get_json(silent=True)
-    # get json request
-
-    json_data = { # for testing
-        'user' : {
-            'x' : 37.50611,
-            'y' : 127.0616346
-        },
-        'devices' : [
-            {
-                'id' : '0001',
-                'x' : 37.5077121,
-                'y' : 127.0624397,
-                'data' : 'something'
-            }
-        ]
-    }
-
-    user_location = (json_data['user']['x'], json_data['user']['y'])
-    # json example : { 'user' : { 'x' : '300' , 'y' : '300' } }
-    # get user_location from json & store as turple (x, y)
-
-    devices_data[str(json_data['devices'][0]['id'])] = (
-        json_data['devices'][0]['data']
-    )
-
-    devices_location[str(json_data['devices'][0]['id'])] = (
-        json_data['devices'][0]['x'], 
-        json_data['devices'][0]['y']
-    )
-    # json example : { 'devices' : { 'id' : '0001', x' : '500', 'y' : '500' }, { ... } }
-    # get device_location from json & store turple (x, y) in dictionary with device id as key
-    # use for statements or something to get more locations from more devices
-
-    circle = { # draw circle on map (user_location as center)
-        'stroke_color': '#0000FF',
-        'stroke_opacity': .5,
-        'stroke_weight': 5,
-        # line(stroke) style
-        'fill_color': '#FFFFFF', 
-        'fill_opacity': .2,
-        # fill style
-        'center': { # set circle to user_location
-            'lat': user_location[0],
-            'lng': user_location[1]
-        }, 
-        'radius': 500 # circle size (50 meters)
-    }
-
-    map = Map(
-        identifier = "map", varname = "map",
-        # set identifier, varname
-        lat = user_location[0], lng = user_location[1], 
-        # set map base to user_location
-        zoom = 15, # set zoomlevel
-        markers = [
-            {
-                'lat': devices_location['0001'][0],
-                'lng': devices_location['0001'][1],
-                'infobox': devices_data['0001']
-            }
-        ], 
-        # set markers to location of devices
-        circles = [circle] # pass circles
-    )
-
-    return render_template('maps.html', map=map) # render template
-
-@app.route('/getdata', methods=['GET', 'POST'])
-def getdata():
-    json_data = requests.get.args('json')
-    return json_data
-    # you can use this to get request with strings and parse json
-    # put data in database or something
-
-if __name__ == '__main__':
-    app.run(debug = True) # run app
